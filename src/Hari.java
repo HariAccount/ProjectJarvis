60eb4156141776dd1971257582d8cc9032800749
Naveen Kumar <programmingwithnaveen@gmail.com>
2023-09-06 10:51:45 +0530

    commit for Hari Testing testcase added

--- src/Hari.java
+++ src/Hari.java
@@ -1,4 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hari {
 
@@ -6,5 +10,16 @@ public class Hari {
     @Test
     public void sample(){
         System.out.println("Test1");
+    }
+
+    @Test
+    public void TC1(){
+        System.setProperty("WebDriver.chrome.driver","C:\\Users\\pc\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://letcode.in/test"); //Opening the url
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
     }
 }
